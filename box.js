5416092d5b1d5518763f90e3192e70e7aa961d99
Oscar <oscar.hilton@mobile-5.com>
2020-02-13 20:54:57 +0000

    progress

--- box.js
+++ box.js
@@ -1,22 +1,33 @@
-function Box(x, y, w, h, image) {
+function Box(x, y, w, h, img) {
   var options = {
     friction: 0.3,
     restitution: 0.6,
   };
-  this.body = Bodies.rectangle(x, y, w, h, options);
+  this.body = Bodies.circle(x, y, w / 2, options);
   this.w = w;
   this.h = h;
+  this.alive = true;
   World.add(world, this.body);
+
+  this.remove = function() {
+    Matter.Composite.remove(world, this.body);
+    this.alive = false;
+  };
 
   this.show = function() {
     var pos = this.body.position;
     var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    rectMode(CENTER);
-    fill(127);
-    image(img, 0, 0, this.w, this.h);
-    pop();
+    if (this.alive) {
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill(127);
+      image(img, -this.h / 2, -this.w / 2, this.w, this.h);
+      pop();
+    }
+    if (pos.y > windowHeight) {
+      this.remove();
+    }
   };
 }
--- sketch.js
+++ sketch.js
@@ -9,10 +9,13 @@ var boxes = [];
 
 var ground;
 var imgs = [];
+let count;
+let circleWidth;
+let maxCircleWidth = 400;
 
 function preload() {
-  for (var i = 0; i < 1; i++) {
-    imgs[i] = loadImage("images/emoji/" + i + ".jpg");
+  for (var i = 0; i < 4; i++) {
+    imgs[i] = loadImage("images/emoji/" + i + ".png");
   }
 }
 
@@ -25,21 +28,27 @@ function setup() {
     isStatic: true,
   };
 
-  ground = Bodies.circle(windowWidth / 2, windowHeight / 2, 40, options);
+  count = 0;
 
-  World.add(world, ground);
+  if (windowWidth / 2 < maxCircleWidth) {
+    circleWidth = windowWidth / 2;
+  } else {
+    circleWidth = maxCircleWidth;
+  }
+
+  middle = Bodies.circle(
+    windowWidth / 2,
+    windowHeight / 2,
+    circleWidth / 2,
+    options
+  );
+
+  World.add(world, middle);
 }
 
 function mousePressed() {
-  boxes.push(
-    new Box(
-      mouseX,
-      mouseY,
-      random(10, 40),
-      random(10, 40),
-      imgs[random(0, imgs.length)]
-    )
-  );
+  const randomHeight = random(1, 2) * 50;
+  boxes.push(new Box(mouseX, mouseY, randomHeight, randomHeight, random(imgs)));
 }
 
 function draw() {
@@ -51,9 +60,25 @@ function draw() {
   noStroke(255);
   fill(170);
   rectMode(CENTER);
-  ellipse(ground.position.x, ground.position.y, 100, 100);
+  ellipse(middle.position.x, middle.position.y, circleWidth, circleWidth);
 
-  // boxes.push(
-  //   new Box(random(0, windowWidth), -10, random(10, 40), random(10, 40))
-  // );
+  const randomHeight = random(1, 2) * 20;
+
+  if (count > 3) {
+    boxes.push(
+      new Box(
+        random(0, windowWidth),
+        -100,
+        randomHeight,
+        randomHeight,
+        random(imgs)
+      )
+    );
+    count = 0;
+  }
+  count++;
+
+  boxes.filter(function(box) {
+    return box.x > windowHeight;
+  });
 }
